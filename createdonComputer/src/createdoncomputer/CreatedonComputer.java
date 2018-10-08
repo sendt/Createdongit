e4499052858952034d985ee77602ad195d96a0aa
Bayram <by.ram.aydnnn@gmail.com>
2018-10-08 11:38:34 +0300

    yeni eklenen ve türetiilen sınıf çalışmalrı

--- createdonComputer/src/createdoncomputer/CreatedonComputer.java
+++ createdonComputer/src/createdoncomputer/CreatedonComputer.java
@@ -9,13 +9,27 @@ package createdoncomputer;
  *
  * @author root
  */
-public class CreatedonComputer {
+public class CreatedonComputer extends T {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    T t1=new T();
+    T t2=new T("birinci string parametere");
+    T t3=new T("birinci parametere ve hemen devamında int deger 55", 55);
     }
     
+}
+class T{
+    public T(){
+        System.out.println("yapılandırıcı metodsuz");
+    }
+    public T(String a){
+        System.out.println("küçücük bir parametrre alan yordam alınana parametere String "+a);
+    }
+    public T(String a,int x){
+        System.out.println("iki parametere alan bir yapılandırcı String "+a+"  ve int deger "+x);
+    }
 }
