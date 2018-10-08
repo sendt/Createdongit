8920a7994d2ad2d819519548efff70d814ff4504
Bayram <by.ram.aydnnn@gmail.com>
2018-10-08 12:45:10 +0300

    i did some examples :)

--- createdonComputer/src/createdoncomputer/CreatedonComputer.java
+++ createdonComputer/src/createdoncomputer/CreatedonComputer.java
@@ -10,26 +10,46 @@ package createdoncomputer;
  * @author root
  */
 public class CreatedonComputer extends T {
+    final  int x;
+    public void ekranabas(String yazi){
+        System.out.println("cratea ekranaa basıldı "+yazi);
+    }
+    public CreatedonComputer(int sayi1){
+        super(sayi1-1);
+        
+        System.out.println(" selam ben Createdoncomputer sınıfı "+sayi1);
+   x=4;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    T t1=new T();
-    T t2=new T("birinci string parametere");
-    T t3=new T("birinci parametere ve hemen devamında int deger 55", 55);
+   CreatedonComputer c1=new CreatedonComputer(5);
+   
+   c1.ekranabas("para"); 
+   System.out.println("Create içindeki static int x degeri : "+c1.x);
+   // T t2=new T("birinci string parametere");
+  //  T t3=new T("birinci parametere ve hemen devamında int deger 55", 55);
+    //    System.out.println(t1.toString());
     }
     
 }
-class T{
-    public T(){
-        System.out.println("yapılandırıcı metodsuz");
+class T extends C{
+    public  void ekranabas(String yazi){ // if i wirte here final i cant ovverding in other extends class
+        System.out.println("class T ekrana basıldı. "+yazi);
     }
-    public T(String a){
-        System.out.println("küçücük bir parametrre alan yordam alınana parametere String "+a);
+    
+    public T(int sayi1){
+        super(sayi1-1);
+        System.out.println("merhaba ben T sınfı "+ sayi1);
     }
-    public T(String a,int x){
-        System.out.println("iki parametere alan bir yapılandırcı String "+a+"  ve int deger "+x);
+    
+   
+}
+class C{
+    public C(int sayi1){
+        System.out.println(" selam ben c sınıfı "+ sayi1);
     }
 }
